cfff9a6d1af06b3d28a25da27d32e73ed39ee76b
RockyRori <rockyrori@ado.com>
2025-07-18 09:39:12 +0800

    [doc]更新文档

--- backend/config.py
+++ backend/config.py
@@ -4,6 +4,7 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 class Config:
+    # 格式："mysql+pymysql://<自己电脑的数据库名称>:<自己电脑的数据库密码>@localhost:3306/docmind?charset=utf8mb4"
     SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:qwertyuiop@localhost:3306/docmind?charset=utf8mb4"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     # 上传与生成文件都放在这里
